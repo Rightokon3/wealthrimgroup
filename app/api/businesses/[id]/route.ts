32be3ad4f38eb14a33af71255bc54d43dbe118f9
ayomikuolarewaju <almaroofolarewaju@gmail.com>
2026-05-24 18:17:52 +0100

    force-dynamic fix

--- app/api/businesses/[id]/route.ts
+++ app/api/businesses/[id]/route.ts
@@ -1,7 +1,7 @@
-export const dynamic = 'force-dynamic';
-
 import { createClient } from '@supabase/supabase-js';
 import { NextRequest, NextResponse } from 'next/server';
+
+export const dynamic = 'force-dynamic';
 
 const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
