039a8a4537d238259a8e81a0ca371051776bfedc
ayomikuolarewaju <almaroofolarewaju@gmail.com>
2026-06-30 06:22:01 +0100

    offline

--- app/offline/page.tsx
+++ app/offline/page.tsx
@@ -1,3 +1,6 @@
+'use client'
+
+
 export default function OfflinePage() {
   return (
     <div className="min-h-screen bg-gradient-to-br from-orange-50 to-red-50 flex items-center justify-center px-4">
